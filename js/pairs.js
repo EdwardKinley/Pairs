3df7ae511ee1d98d82e5e4d9ce8696db3372c448
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-05-18 00:31:30 +0100

    sets up initial page showing number of cards options

--- js/pairs.js
+++ js/pairs.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM content loaded');
+
+  cardNumberOptions = [8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52];
+
+  showCardNumberOptions();
+
+  function showCardNumberOptions() {
+    const main = document.querySelector('.main');
+    const optionsSpace = document.createElement('div');
+    optionsSpace.className = 'optionsSpace';
+    main.appendChild(optionsSpace);
+    for (i=0; i<cardNumberOptions.length; i++) {
+      const option = document.createElement('div');
+      option.className = 'cardNumberOption';
+      option.textContent = cardNumberOptions[i];
+      optionsSpace.appendChild(option);
+      option.addEventListener('click', () => {
+        console.log(option.textContent);
+      })
+    }
+  }
+
 })
